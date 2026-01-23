d7332f964cb5ae21bcbe7913988610ccf454074c
anirudh-naruka <anirudh.naruka@juspay.in>
2026-01-23 16:48:13 +0530

    Revert "fix: updated jest"
    This reverts commit dbcfb21d1fab0df2328831ef8c2e9fe53ed75b7a.

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,19 +1,6 @@
 // Load test environment variables
 require('dotenv').config({ path: '.env.test' });
 
-// Polyfill browser APIs required by pdf-parse/pdfjs-dist
-// DOMMatrix is used internally by pdfjs-dist but not actually needed for text extraction
-global.DOMMatrix = class DOMMatrix {
-  constructor() {
-    this.a = 1;
-    this.b = 0;
-    this.c = 0;
-    this.d = 1;
-    this.e = 0;
-    this.f = 0;
-  }
-};
-
 // Increase timeout for database tests
 jest.setTimeout(10000);
 
